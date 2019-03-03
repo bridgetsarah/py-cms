bbb0864eddf52d37f846d855880fc992b4c16bb8
Bridget Sarah <msbskelly@gmail.com>
2019-03-03 22:15:52 +0000

    Added: Login Function

---
+++ login.py
@@ -0,0 +1,8 @@
+# Login Function
+
+def login(login, password):
+    username = ('username'),
+    if [username] is True:
+        print('Success!')
+    elif [password] is False:
+        print ('Invalid Password!')
